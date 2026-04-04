03048849057ca98a84bd55c827a3611b3a16be30
Matt Kielar <mattk101@student.ubc.ca>
2026-04-03 23:30:10 -0700

    change LP class to accept dataframes instead of arrays

--- src/ExoSmoothLP.py
+++ src/ExoSmoothLP.py
@@ -7,16 +7,26 @@ from pyGAM import LinearGAM, f
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-class ExoSmoothLP:
+class LinearProjections:
 
-    def __init__(self, Y: np.ndarray, x: np.ndarray, p: int, H: int):
+    def __init__(self, data: pd.DataFrame, shock: str, endog: list[str] = None, p: int, H: int):
+
+        Y_df = data.drop(columns = shock) if endog is None else data[endog_cols]
+        Y = Y_df.to_numpy()
+        contemp = data.columns[:data.columns.get_loc(shock)].tolist()
+        Z = data[contemp].to_numpy()
+        x = data[shock_col].to_numpy()
+        var_names = Y_df.columns.tolist()
+
         self.Y = Y
         self.x = x
+        self.Z = Z
         self.p = p
         self.H = H
         self.T, self.k = Y.shape
         self.beta = None
         self.irf = None
+        self.var_names = var_names
 
     def _build_lag_matrix(self):
         # helper function to build lag matrix W
@@ -26,16 +36,34 @@ class ExoSmoothLP:
             out[lag:, (lag - 1) * k : lag * k] = self.Y[:-lag]
         return out[p:]
 
-    def estimate(self):
+    def ExoLP(self):
         W = self._build_lag_matrix()
-        self.irf = np.empty((self.H + 1, self.k))
+        self.beta = np.empty((self.H + 1, self.k))
 
         for j in range(self.k):
             y = self.Y[:, j]
             for h in range(self.H + 1):
                 T_h = self.T - self.p - h
-                X = sm.add_constant(np.append(self.x[:T_h].reshape(-1, 1), W[:T_h, :], axis = 1))
-                lp_mod = sm.OLS(endog = y[self.p + h:], exog = X)
+                X_h = np.append(self.x[:T_h].reshape(-1, 1), W[:T_h, :], axis = 1)
+                X_h = sm.add_constant(X_h)
+                y_h = y[self.p + h: ]
+                lp_mod = sm.OLS(endog = y_h, exog = X_h)
+                lp_fit = lp_mod.fit(cov_type = 'HAC', cov_kwds = {'maxlags': h})
+                self.beta[h, j] = lp_fit.params[1]
+        return self.beta
+    
+    def EndoLP(self):
+        W = self._build_lag_matrix()
+        self.beta = np.empty((self.H + 1, self.k))
+
+        for j in range(self.k):
+            y = self.Y[:, j]
+            for h in range(self.H + 1):
+                T_h = self.T - self.p - h
+                X_h = np.hstack([self.x[:T_h], self.Z[:T_h], self.W[:T_h]])
+                X_h = sm.add_constant(X_h)
+                y_h = y[self.p + h: ]
+                lp_mod = sm.OLS(endog = y_h, exog = X_h)
                 lp_fit = lp_mod.fit(cov_type = 'HAC', cov_kwds = {'maxlags': h})
                 self.beta[h, j] = lp_fit.params[1]
         return self.beta
